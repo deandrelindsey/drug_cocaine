e51e448cdfee0f98dab792cf382577a3d097e4b8
William Lindsey <deandrelindsey16@gmail.com>
2025-07-08 13:09:09 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,4 +1,19 @@
-Config = {
-    DrugName = 'drug_cocaine',
-    Enabled = true
+Config = {}
+
+Config.Stages = {
+    leaf = 'coca_leaf',
+    paste = 'coca_paste',
+    powder = 'cocaine_powder',
+    crack = 'cocaine_crack',
+    fishscale = 'cocaine_fishscale'
 }
+
+Config.CrackExplosionChance = 15   -- % chance for explosion
+Config.FishscaleExplosionChance = 30
+
+Config.CivilianRestriction = true
+Config.CivilianFailureChance = 75 -- % chance for civ to fail when making powder
+Config.AllowedJobs = { 'gang', 'corruptcop', 'drugdealer' }
+
+Config.RobberyChanceCiv = 90
+Config.RobberyChanceCrim = 10
