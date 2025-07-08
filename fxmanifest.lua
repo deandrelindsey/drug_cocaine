91e6e1ebabde2f886ed1454f8cb910dc73217e97
William Lindsey <deandrelindsey16@gmail.com>
2025-07-08 13:08:36 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,4 +7,5 @@ version '1.0.0'
 shared_script 'config.lua'
 shared_script 'locales/en.lua'
 client_script 'client/main.lua'
-server_script 'server/main.lua'
+server_script '@oxmysql/lib/MySQL.lua',
+    'server/main.lua'
