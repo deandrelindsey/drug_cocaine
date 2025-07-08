0df187aa501908a2527df675e441892fa592d1fc
William Lindsey <deandrelindsey16@gmail.com>
2025-07-08 13:09:28 -0400

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -1,3 +1,18 @@
-RegisterCommand('usedrug', function()
-    print('Drug used')
-end, false)
+RegisterNetEvent('drug_cocaine:useEffect', function(stage)
+    local ped = PlayerPedId()
+    local effect = "DrugsTrevorClownsFight"
+
+    if stage == 'crack' or stage == 'fishscale' then
+        effect = "DrugsMichaelAliensFight"
+    end
+
+    TaskStartScenarioInPlace(ped, "WORLD_HUMAN_SMOKING", 0, true)
+    Wait(3000)
+    ClearPedTasks(ped)
+
+    StartScreenEffect(effect, 0, true)
+    ShakeGameplayCam("DRUNK_SHAKE", 1.0)
+    Wait(25000)
+    StopScreenEffect(effect)
+    ShakeGameplayCam("DRUNK_SHAKE", 0.0)
+end)
